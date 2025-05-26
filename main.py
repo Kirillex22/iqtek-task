278c181e850deb1771b8b46ce0e57ee851ce2e9a
Кирилл <126565102+Kirillex22@users.noreply.github.com>
2025-05-26 15:15:21 +0600

    Фикс разбиения на файлы

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+from src.plugins.user.routers.UserRouter import user_router
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs")
+
+
+app.include_router(user_router)
